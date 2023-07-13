c6e788895cd86df51de930459b6a7fa9ccc7685f
ZeyuLiu0706 <58979380+ZeyuLiu0706@users.noreply.github.com>
2023-07-13 14:54:09 +0800

    Update train_ml10m.py

--- train_ml10m.py
+++ train_ml10m.py
@@ -7,20 +7,12 @@ import torch.nn as nn
 from LibMTL.utils.timer import TimeRecorder
 import numpy as np
 
-
-
-
-
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Configuration for LibMTL')
     parser.add_argument('--dataset', default='ml10m', type=str, help='dataset_name')
     parser.add_argument('--method', default='EW', type=str, help='the weighting methods: Nash, PCGrad')
     args = parser.parse_args()
     return args
-
-
 
 if __name__ == "__main__":
     yaml_config = parse_args()
